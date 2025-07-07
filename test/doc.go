96ef0f1a6b971eec5102ef2b518a9179730a069e
Ryuichi <ryuichi.hayashi@gmail.com>
2025-07-07 14:43:46 +0900

    fix: add doc.go to test package to prevent build errors
    - Add package documentation file to test directory
    - This prevents 'no packages' error when test directory is scanned
    - Integration tests still require -tags=integration to run

---
+++ test/doc.go
@@ -0,0 +1,4 @@
+// Package test contains integration tests for the envy project.
+// These tests are not included in regular unit test runs and require
+// specific build tags to execute.
+package test
